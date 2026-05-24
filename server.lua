0e4183c422a0835280f34d0b598d0bd1916ef03f
ArtMinerCZ <62687944+ArtMinerCZ@users.noreply.github.com>
2026-05-24 19:21:34 +0200

    Fix URL in update_sw function for server.lua

--- server.lua
+++ server.lua
@@ -214,7 +214,7 @@ end
 
 local function update_sw()
     fs.delete("startup.lua")
-    shell.run("wget https://raw.githubusercontent.com/ArtMinerCZ/CC-Tweaked-Intranet/refs/heads/main/startup.lua startup.lua")
+    shell.run("wget https://raw.githubusercontent.com/ArtMinerCZ/CC-Tweaked-Intranet/refs/heads/main/server.lua startup.lua")
     shell.run("startup.lua")
 end
 
