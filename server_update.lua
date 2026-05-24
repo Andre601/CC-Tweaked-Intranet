e85c405e5ec37945c8b72d79d5bc2271d6bca7af
ArtMinerCZ <62687944+ArtMinerCZ@users.noreply.github.com>
2026-05-24 17:13:20 +0200

    Update server_update.lua to download server.lua

--- server_update.lua
+++ server_update.lua
@@ -1,3 +1,3 @@
 fs.delete("startup.lua")
-shell.run("wget https://raw.githubusercontent.com/ArtMinerCZ/CC-Tweaked-Intranet/refs/heads/main/startup.lua startup.lua")
+shell.run("wget https://raw.githubusercontent.com/ArtMinerCZ/CC-Tweaked-Intranet/refs/heads/main/server.lua startup.lua")
 shell.run("startup.lua")
